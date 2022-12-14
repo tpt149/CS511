8e63cdd72a9da4e01b09eb90b89afdad02acb329
Thao Tran <88588832+tpt149@users.noreply.github.com>
2022-12-14 21:34:54 +0700

    up

---
+++ Musicapp/Musicapp/song.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Musicapp
+{
+    partial class song
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(song));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labName = new System.Windows.Forms.Label();
+            this.labType = new System.Windows.Forms.Label();
+            this.lab_author = new System.Windows.Forms.Label();
+            this.labDur = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labName
+            // 
+            this.labName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labName.ForeColor = System.Drawing.Color.Sienna;
+            this.labName.Location = new System.Drawing.Point(101, 24);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(414, 23);
+            this.labName.TabIndex = 1;
+            this.labName.Text = "Tên bài hát";
+            // 
+            // labType
+            // 
+            this.labType.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labType.ForeColor = System.Drawing.Color.Sienna;
+            this.labType.Location = new System.Drawing.Point(469, 24);
+            this.labType.Name = "labType";
+            this.labType.Size = new System.Drawing.Size(99, 23);
+            this.labType.TabIndex = 2;
+            this.labType.Text = "Thể loại";
+            // 
+            // lab_author
+            // 
+            this.lab_author.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_author.ForeColor = System.Drawing.Color.Sienna;
+            this.lab_author.Location = new System.Drawing.Point(603, 24);
+            this.lab_author.Name = "lab_author";
+            this.lab_author.Size = new System.Drawing.Size(275, 23);
+            this.lab_author.TabIndex = 3;
+            this.lab_author.Text = "Văn Mai Hương, Hứa Kim Tuyền";
+            // 
+            // labDur
+            // 
+            this.labDur.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labDur.ForeColor = System.Drawing.Color.Sienna;
+            this.labDur.Location = new System.Drawing.Point(929, 24);
+            this.labDur.Name = "labDur";
+            this.labDur.Size = new System.Drawing.Size(99, 23);
+            this.labDur.TabIndex = 4;
+            this.labDur.Text = "Thời gian";
+            // 
+            // song
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labDur);
+            this.Controls.Add(this.lab_author);
+            this.Controls.Add(this.labType);
+            this.Controls.Add(this.labName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "song";
+            this.Size = new System.Drawing.Size(1100, 75);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.Label labType;
+        private System.Windows.Forms.Label lab_author;
+        private System.Windows.Forms.Label labDur;
+    }
+}
---
+++ Musicapp/Musicapp/song.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Musicapp
+{
+    public partial class song : UserControl
+    {
+        public song()
+        {
+            InitializeComponent();
+        }
+        /*public void loadFeedback(int star)
+        {
+            switch (star)
+            {
+                case 1:
+                    pb_str2.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+                    pb_str3.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+                    pb_str4.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+                    pb_str5.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+
+                    pb_str1.Image = Image.FromFile(@"..//..//img/icon/yellow_star.png");
+                    lbl_core.Text = "1/5";
+                    break;
+                case 2:
+                    //change yellow stars to white
+                    pb_str3.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+                    pb_str4.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+                    pb_str5.Image = Image.FromFile(@"..//..//img/icon/white_star.png");
+
+                    pb_str1.Image = Image.FromFile(@"..//..//img/icon/yellow_star.png");
+                    pb_str2.Image = Image.FromFile(@"..//..//img/icon/yellow_star.png");
+                    lbl_core.Text = "2/5";
+                    break;
+                case 3:
+                    //change yellow stars to white
+                    pb_str4.Image = Image.FromFile(@"..//..//icon/white_star.png");
+                    pb_str5.Image = Image.FromFile(@"..//..//icon/white_star.png");
+
+                    pb_str1.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str2.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str3.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    lbl_core.Text = "3/5";
+                    break;
+                case 4:
+                    //change yellow star to white
+                    pb_str5.Image = Image.FromFile(@"..//..//icon/white_star.png");
+
+                    pb_str1.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str2.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str3.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str4.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    lbl_core.Text = "4/5";
+                    break;
+                case 5:
+                    pb_str1.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str2.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str3.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str4.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    pb_str5.Image = Image.FromFile(@"..//..//icon/yellow_star.png");
+                    lbl_core.Text = "5/5";
+                    break;
+                default:
+                    break;
+            }
+        }
+        string a = "";
+        public string ItemImage
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                //MessageBox.Show(ItemType);
+                ptr_Item.BackgroundImage = Image.FromFile(@"..//..//img" + "/" + ItemType + "/" + value + ".png");
+                a = value;
+            }
+        }*/
+        public string ItemName
+        {
+            get
+            {
+                return labName.Text;
+            }
+            set
+            {
+                labName.Text = value;
+            }
+        }
+        public string ItemType
+        {
+            get
+            {
+                return labType.Text;
+            }
+            set
+            {
+                labType.Text = value;
+            }
+        }
+
+        public string ItemAuthor
+        {
+            get
+            {
+                return lab_author.Text;
+            }
+            set
+            {
+                lab_author.Text = value;
+            }
+        }
+        public string ItemDuration
+        {
+            get
+            {
+                return labDur.Text;
+            }
+            set
+            {
+                labDur.Text = value;
+            }
+        }
+        /*public string ItemLyric;
+
+        string aflat = "false";
+        public string ItemLove
+        {
+            get
+            {
+                return aflat;
+            }
+            set
+            {
+                if (value == "true")
+                {
+                    picLove.BackgroundImage = Image.FromFile(@"..//..//icon/redheart.png");
+                    aflat = value;
+                }
+            }
+        }
+
+        private void picLove_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Form1.songs.Name.Count; i++)
+            {
+                if (Form1.songs.Name[i] == ItemName)
+                {
+                    if (Form1.songs.Love[i] != "false")
+                    {
+                        picLove.BackgroundImage = Image.FromFile(@"..//..//icon/heart.png");
+                        Form1.songs.Love[i] = "false";
+                    }
+                    else
+                    {
+                        picLove.BackgroundImage = Image.FromFile(@"..//..//icon/redheart.png");
+                        Form1.songs.Love[i] = "true";
+                    }
+                }
+            }*/
+    }
+}
